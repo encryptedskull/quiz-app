2662eafede64a7960b5a9a153fda23ab45b54ae7
Mohammad Noor Haider <noorhaider381@gmail.com>
2024-08-02 19:02:36 +0500

    fixed fetching issue

--- src/pages/GetPage.jsx
+++ src/pages/GetPage.jsx
@@ -8,7 +8,10 @@ export default function GetPage({ bigState, setBigState }) {
 
   const fetchData = async () => {
     try {
-      const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
+      const genAI = new GoogleGenerativeAI(
+        "AIzaSyAaEQ4bKIuYzVpbARJSIJbiJzhuBbQ3jpw"
+      );
+      // const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
 
       const model = genAI.getGenerativeModel({
         model: "gemini-1.5-flash",
